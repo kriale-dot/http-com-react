b54077c0c319186097688d3363f47c85dedc2754
Kriale <contato@kriale.com.br>
2026-06-15 17:01:20 -0300

    enviar dados

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
 
   const [products, setProducts] = useState([]);
 
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
   // 1 - resgatando dados
   useEffect(() => {
     async function fetchData() {
@@ -20,7 +23,20 @@ function App() {
     fetchData();
   }, []);
 
+  // 2 - adição de produtos
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const product = { name, price }
 
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    });
+
+  }
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
@@ -29,6 +45,20 @@ function App() {
           <li key={product.id}>{product.name} - R${product.price}</li>
         ))}
       </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+          </label>
+
+          <label>
+            Preço:
+            <input type="number" name="price" value={price} onChange={(e) => setPrice(e.target.value)} />
+          </label>
+          <input type="submit" value="Criar" />
+        </form>
+      </div>
     </div>
   );
 }
