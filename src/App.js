bdd9cdbfdef1fff2082cb2841a30d82374546b80
Kriale <contato@kriale.com.br>
2026-06-15 16:14:39 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+
+import './App.css';
+import { useState, useEffect } from "react";
+
+const url = "http://192.168.15.8:3000/products"
+
+function App() {
+
+  const [products, setProducts] = useState([]);
+
+  // 1 - resgatando dados
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url)
+
+      const data = await res.json()
+
+      setProducts(data)
+    }
+    fetchData();
+  }, []);
+
+
+  return (
+    <div className="App">
+      <h1>Lista de produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name} - R${product.price}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
